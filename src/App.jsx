e591452632bb30051d11215728a9ddeceefc04b0
Назар <ratbekovnazar@gmail.com>
2026-01-08 20:47:30 +0600

    тест

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import Block from "./components/Block"
+import Quiz from "./components/Quiz";
+
 function App() {
   return (
-      <div> 
-        <h1>Vite + React</h1>
-        /block
-      </div>
-  )
+    <Quiz
+      question="Сколько будет 3 + 3?"
+      options={["5", "6", "7", "8"]}
+      correctIndex={1}
+    />
+  );
 }
 
-export default App
+export default App;
